5447bef319d9f06d9e71a05302c63901cd5e592e
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-05-14 19:11:03 -0400

    jest config

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types'
+
+export default async (): Promise<Config.InitialOptions> => {
+  return {
+    preset: 'ts-jest',
+    verbose: true,
+    testMatch: ['**/src/**/*test.ts'],
+  }
+}
