0822f205d6a0dfeab86fec453363ae9f1ae4c834
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-05-14 18:48:27 -0400

    added nodeless type and guard

--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,20 @@ export type JSONValue =
   | JSONArray
   | null
 
+export type JSONNodeless = string | number | boolean | null
+
+export const isNodeless = (elem: JSONValue): elem is JSONNodeless => {
+  if (elem === null) return true
+  if (
+    typeof elem === 'string' ||
+    typeof elem === 'number' ||
+    typeof elem === 'boolean'
+  )
+    return true
+
+  return false
+}
+
 export type JSONObject = {
   [x: string]: JSONValue
 }
