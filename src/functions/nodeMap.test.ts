c84014a5119359ebdab51688e0e5902690dd5568
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-05-14 19:11:33 -0400

    nodemap test

---
+++ src/functions/nodeMap.test.ts
@@ -0,0 +1,9 @@
+import { nodeMap } from './nodeMap'
+
+test('identity function works with nodeless iterables', () => {
+  const nodeMapIdentityFunc = nodeMap((x) => x)
+  expect(nodeMapIdentityFunc(1)).toBe(1)
+  expect(nodeMapIdentityFunc(true)).toBe(true)
+  expect(nodeMapIdentityFunc('hi')).toBe('hi')
+  expect(nodeMapIdentityFunc(null)).toBe(null)
+})
