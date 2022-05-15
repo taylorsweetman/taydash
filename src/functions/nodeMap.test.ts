469c9650514c909257b12368b8e4cf39671142fe
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-05-15 11:05:25 -0400

    added base cases of empty obj or list

--- src/functions/nodeMap.test.ts
+++ src/functions/nodeMap.test.ts
@@ -2,8 +2,11 @@ import { nodeMap } from './nodeMap'
 
 test('identity function works with nodeless iterables', () => {
   const nodeMapIdentityFunc = nodeMap((x) => x)
+
   expect(nodeMapIdentityFunc(1)).toBe(1)
   expect(nodeMapIdentityFunc(true)).toBe(true)
   expect(nodeMapIdentityFunc('hi')).toBe('hi')
   expect(nodeMapIdentityFunc(null)).toBe(null)
+  expect(nodeMapIdentityFunc({})).toEqual({})
+  expect(nodeMapIdentityFunc([])).toEqual([])
 })
--- src/functions/nodeMap.ts
+++ src/functions/nodeMap.ts
@@ -1,8 +1,12 @@
 import { isNodeless, JSONValue } from '../types'
+import { isPlainObject, isArray, isEmpty } from 'lodash/fp'
 
 export const nodeMap =
   (func: (a: JSONValue) => JSONValue) =>
   (iterable: JSONValue): JSONValue => {
     if (isNodeless(iterable)) return func(iterable)
+
+    if (isPlainObject(iterable) && isEmpty(iterable)) return {}
+    if (isArray(iterable) && isEmpty(iterable)) return []
     return null
   }
