ce2f7e9c1ce7fdea9f135ce661857ae4125e2b88
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-05-15 12:25:15 -0400

    refactored nodeMap

--- src/functions/nodeMap.ts
+++ src/functions/nodeMap.ts
@@ -1,23 +1,34 @@
-import { isNodeless, JSONObject, JSONValue } from '../types'
-import { isPlainObject, isArray } from 'lodash/fp'
+import { isNodeless, JSONArray, JSONObject, JSONValue } from '../types'
+import { isPlainObject, isArray, entries, pipe, reduce, map } from 'lodash/fp'
+
+const handleArray =
+  (mapWithAppliedFunc: (a: JSONValue) => JSONValue) =>
+  (iterable: JSONArray): JSONArray =>
+    map(mapWithAppliedFunc)(iterable)
+
+const handleObject =
+  (mapWithAppliedFunc: (a: JSONValue) => JSONValue) =>
+  (iterable: JSONObject): JSONObject =>
+    pipe(
+      entries,
+      reduce(
+        (accum, [key, val]) => ({ ...accum, [key]: mapWithAppliedFunc(val) }),
+        {} as JSONObject
+      )
+    )(iterable)
 
 export const nodeMap =
   (func: (a: JSONValue) => JSONValue) =>
   (iterable: JSONValue): JSONValue => {
     if (isNodeless(iterable)) return func(iterable)
 
-    const mapWithAppliedFunc = nodeMap(func)
+    const nodeMapWithAppliedFunction = nodeMap(func)
 
-    if (isPlainObject(iterable)) {
-      return Object.entries(iterable).reduce((accum, [key, val]) => {
-        accum[key] = mapWithAppliedFunc(val)
-        return { ...accum, [key]: mapWithAppliedFunc(val) }
-      }, {} as JSONObject)
-    }
+    if (isArray(iterable))
+      return handleArray(nodeMapWithAppliedFunction)(iterable)
 
-    if (isArray(iterable)) {
-      return iterable.map((val) => mapWithAppliedFunc(val))
-    }
+    if (isPlainObject(iterable))
+      return handleObject(nodeMapWithAppliedFunction)(iterable)
 
     return null
   }
