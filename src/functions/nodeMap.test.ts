94b38681ee40a6e2876b1ff33ba9247cb48563a2
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-05-15 11:25:42 -0400

    unit test refactor

--- src/functions/nodeMap.test.ts
+++ src/functions/nodeMap.test.ts
@@ -1,12 +1,28 @@
+import { JSONArray, JSONNodeless, JSONObject } from '../types'
 import { nodeMap } from './nodeMap'
 
-test('identity function works with nodeless iterables', () => {
+describe('nodeMap', () => {
   const nodeMapIdentityFunc = nodeMap((x) => x)
 
-  expect(nodeMapIdentityFunc(1)).toBe(1)
-  expect(nodeMapIdentityFunc(true)).toBe(true)
-  expect(nodeMapIdentityFunc('hi')).toBe('hi')
-  expect(nodeMapIdentityFunc(null)).toBe(null)
-  expect(nodeMapIdentityFunc({})).toEqual({})
-  expect(nodeMapIdentityFunc([])).toEqual([])
+  it.each([
+    [1, 1],
+    [true, true],
+    ['hi', 'hi'],
+    [null, null],
+  ])(
+    'should use identity function on nodeless iterables correctly: %s -> %s',
+    (iter: JSONNodeless, expected: JSONNodeless) => {
+      expect(nodeMapIdentityFunc(iter)).toBe(expected)
+    }
+  )
+
+  it.each([
+    [{}, {}],
+    [[], []],
+  ])(
+    'should return empty object or list when receiving empty object or list: %s -> %s',
+    (iter: JSONObject | JSONArray, expected: JSONObject | JSONArray) => {
+      expect(nodeMapIdentityFunc(iter)).toEqual(expected)
+    }
+  )
 })
