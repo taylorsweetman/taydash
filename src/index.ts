bbca0b742110e293a4b9f67afdb388739b44e19c
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-05-21 16:33:43 -0400

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { leafNodeMap } from './functions/leafNodeMap'
-import { JSONArray, JSONLeaf, JSONObject, JSONValue, isLeaf } from './types'
-
+export { leafNodeMap } from './functions/leafNodeMap'
 export {
-  leafNodeMap,
   type JSONArray,
   type JSONLeaf,
   type JSONObject,
   type JSONValue,
   isLeaf,
-}
+} from './types'
