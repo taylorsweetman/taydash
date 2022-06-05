54f1e6e41826e62fd55d8d828fa9c408c55493fa
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-06-05 10:19:31 -0400

    roundIfNum (#9)
    * added coverage to test command
    
    * coverage improvement
    
    * roundIfNum with tests
    
    * Maybe type
    
    * export new work
    
    * bumped version number
    
    * updated readme

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 export { mapLeafNodes, _mapLeafNodes } from './functions/mapLeafNodes'
+export { roundIfNum, _roundIfNum } from './functions/roundIfNum'
 export {
   type JSONArray,
   type JSONLeaf,
   type JSONObject,
   type JSONValue,
   isLeaf,
+  type Maybe,
 } from './types'
